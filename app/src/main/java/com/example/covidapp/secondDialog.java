e6cea7045e4971dfeb9f9166cc1a79d2a88e8889
RandBY <younis.rand96@gmail.com>
2020-10-14 14:34:08 +1030

    init

---
+++ app/src/main/java/com/example/covidapp/secondDialog.java
@@ -0,0 +1,25 @@
+package com.example.covidapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class secondDialog extends AppCompatDialogFragment{
+
+    public Dialog onCreateDialog (Bundle savedInstanceState)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Verification Status").setMessage("Your Verification Code has been accepted and your status is shared").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+
+        return builder.create();
+    }
+
+}
