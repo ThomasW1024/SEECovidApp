7a06089325449c0eaee5a2649cd2ad89b006d0bd
Thomas <wongthomas03@gmail.com>
2020-10-24 19:45:39 +1030

    fix HTTPS communication

--- app/src/main/java/com/example/covidapp/MyApplication.java
+++ app/src/main/java/com/example/covidapp/MyApplication.java
@@ -1,9 +1,30 @@
 package com.example.covidapp;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
 import androidx.work.Configuration;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Objects;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 public class MyApplication extends Application implements Configuration.Provider {
 
@@ -11,6 +32,28 @@ public class MyApplication extends Application implements Configuration.Provider
     public void onCreate() {
         super.onCreate();
         //Parse SDK stuff goes here
+        try {
+            HttpsURLConnection.setDefaultSSLSocketFactory(getSocketFactory(this));
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    if (Objects.equals("10.0.2.2", hostname)) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -18,6 +61,47 @@ public class MyApplication extends Application implements Configuration.Provider
         Configuration myConfig = new Configuration.Builder()
                 .setMinimumLoggingLevel(Log.INFO)
                 .build();
+//        //initialize WorkManager
+//        WorkManager.initialize(this, myConfig);
         return myConfig;
+    }
+
+    // From MaxMxx at https://stackoverflow.com/questions/17045795/making-a-https-request-using-android-volley
+    public SSLSocketFactory getSocketFactory(Context context)
+            throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+
+        // Load CAs from an InputStream (could be from a resource or ByteArrayInputStream or ...)
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+        InputStream caInput = new BufferedInputStream(context.getResources().openRawResource(R.raw.server));
+        // I paste my myFile.crt in raw folder under res.
+        Certificate ca = null;
+
+        //noinspection TryFinallyCanBeTryWithResources
+        try {
+            ca = cf.generateCertificate(caInput);
+            System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } finally {
+            caInput.close();
+        }
+
+        // Create a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        // Create a TrustManager that trusts the CAs in our KeyStore
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        // Create an SSLContext that uses our TrustManager
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+
+        return sslContext.getSocketFactory();
     }
 }
--- app/src/main/java/com/example/covidapp/httprequest/HttpRequestFactory.java
+++ app/src/main/java/com/example/covidapp/httprequest/HttpRequestFactory.java
@@ -1,11 +1,15 @@
 package com.example.covidapp.httprequest;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.covidapp.ephId.EphemeralGenerator;
 
@@ -17,37 +21,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class HttpRequestQueue {
-    private static HttpRequestQueue instance;
-    private RequestQueue requestQueue;
+public class HttpRequestFactory {
+    private static HttpRequestFactory instance;
     private static Context ctx;
 
-    private HttpRequestQueue(Context context) {
+    private HttpRequestFactory(Context context) {
         ctx = context;
-        requestQueue = getRequestQueue();
     }
 
-    public static synchronized HttpRequestQueue getInstance(Context context) {
-        if (instance == null) {
-            instance = new HttpRequestQueue(context);
+
+    public static synchronized HttpRequestFactory getInstance(Context context) {
+        if(instance == null) {
+            instance = new HttpRequestFactory(context);
         }
         return instance;
     }
 
-    public RequestQueue getRequestQueue() {
-        if (requestQueue == null) {
-            // getApplicationContext() is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
-        }
-        return requestQueue;
+    // usage: HttpRequestFactory.getInstance(HomeActivity.this).sendRequest();
+    public void sendRequest() {
+        Log.e("HTTP", "clickme");
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(ctx);
+        String url = "https://10.0.2.2:3000/";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.e("HTTP", response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("HTTP", error.getLocalizedMessage());
+            }
+        });
+        queue.add(stringRequest);
+
     }
 
-    public <T> void addToRequestQueue(Request<T> req) {
-        getRequestQueue().add(req);
-    }
 
-    // TODO Put it in Activity / service
+
+    // TODO replicate from above example
 //    public boolean getCasesAndMatch(){
 //        boolean ifMatched = false;
 //        RequestFuture<JSONArray> future = RequestFuture.newFuture();
